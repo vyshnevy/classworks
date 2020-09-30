808547686e08381cf76863e451991f277655697b
vishnevy <vishnevyd@gmail.com>
2020-09-30 09:44:33 +0300

    TEXXXT

--- linear/src/Ctrl1/Main.java
+++ linear/src/Ctrl1/Main.java
@@ -1,4 +1,31 @@
 package Ctrl1;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        double weight = s.nextDouble();
+        double price = 30;
+        if(weight <=1 ) {
+            System.out.println("Current price" + price);
+
+        }else if(weight<2&weight>1 ){price += 5;
+            System.out.println("Current price" + price);
+
+        }else if(weight>2&weight<5){price += 20;
+            System.out.println("Current price" + price);
+
+        }else if(weight>5&weight<10){price += 40;
+            System.out.println("Current price" + price);
+
+        }else{
+            price = 100 + (weight - 10)*2;
+
+            System.out.println("Current price" + price);
+
+        }
+
+    }
+
 }
