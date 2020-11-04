460526f6bbffc19ba0df0ced38493602458ae5b0
vishnevy <vishnevyd@gmail.com>
2020-11-04 10:49:32 +0200

    PRT1

--- linear/src/PRfrom9to11/PR111.java
+++ linear/src/PRfrom9to11/PR111.java
@@ -8,35 +8,36 @@ public class PR111 {
         int minI = 0;
         int f1 = 0;
         int f2 = 0;
-        int counter =-1;
+        int counter =0;
         int[] r1 =new int[8];
-        int[] r = new int[10];
-        for (int i = 0, n = r.length; i < n; i++) {
-            r[i] = (int) (Math.random() * 100 * Math.pow(-1, i));}
+        int[] r = {-5,9,-5,6,7,-3,-4,5,9,7};
+
         for (int i = 0, n = r.length; i < n; i++) {
             System.out.print(r[i]+", ");
         }
+        System.out.println();
         for (int i = 0, n = r.length; i < n; i++){
             if (min > r[i]) {
             min = r[i];
-            minI = i;
+            minI =i ;
         }
         if (max < r[i]) {
             max = r[i];
             maxI = i;}
         }
         for (int i = 0, n = r.length; i < n; i++) {
-            if(r[i]==min& i!=minI){f1 = i;}
+            if(r[i]==min & i!=minI){f1 = i;}
             if(r[i]==max& i!=maxI){f2 = i;}
         }
-        for (int i = 0, n = r1.length; i < n; i++) {
-        if(i==f2|i==f1){
-        }else{counter++;
-           r1[counter]=r[i];
-        }
+        for (int i = 0, n = r1.length; i < n; i++)
+            if (i == f1 | i == f2) {
+            } else {
+                r1[counter] = r[i];
+                counter++;
+            }
+        System.out.println(f1+", "+f2);
+        System.out.println(minI+", "+maxI);
 
-
-        }
         for (int i = 0, n = r1.length; i < n; i++) {
             System.out.println(r1[i]);
         }
