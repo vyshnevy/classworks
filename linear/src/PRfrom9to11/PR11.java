51ef344e49e9b1a7edd1dae2cd8f836e4bfb3602
vishnevy <vishnevyd@gmail.com>
2020-11-04 09:55:57 +0200

    PRT

--- linear/src/PRfrom9to11/PR11.java
+++ linear/src/PRfrom9to11/PR11.java
@@ -4,6 +4,8 @@ public class PR11 {
     public static void main(String[] args) {
         int p = 0;
         int n = 0;
+        int x =0;
+        int y =0;
         int[] r = new int[20];
         for (int i = 0, l = r.length; i < l; i++) {
             r[i] = (int) (Math.random() * 100 * Math.pow(-1, i));}
@@ -14,9 +16,10 @@ public class PR11 {
         int[] pos = new int[r.length-n];
         int[] neg = new int[r.length-p];
         for (int i = 0, l = r.length; i < l; i++) {
-            if(r[i]<0){neg[i]=r[i];}
-            if(r[i]>=0){pos[i]=r[i];}
-        } for (int i = 0, l = pos.length; i < l; i++) {System.out.println();}
-     for (int i = 0, l = neg.length; i < l; i++) {System.out.println();}
+            if(r[i]<0){ neg[x]=r[i];x++;}
+            if(r[i]>=0){pos[y]=r[i];y++;}
+        } for (int i = 0, l = pos.length; i < l; i++) {System.out.println("    "+pos[i]);}
+        System.out.println("----------");
+     for (int i = 0, l = neg.length; i < l; i++) {System.out.println("    "+neg[i]);}
     }
 }
